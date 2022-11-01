2b7f7009205490a1df44cf635460e862822bab99
Abhishek kumar <109082907+Abhishek6388@users.noreply.github.com>
2022-11-01 20:12:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+
+import React, { useEffect, useState } from "react";
 import "../styles/App.css";
+// import "./styles.css"
 
 var colors = [
   '#16a085',
@@ -18,10 +20,54 @@ var colors = [
 
 const App = () => {
 
+  const [getData,setData] = useState({
+    quote:'',
+    author:''
+  })
+  const [count,setCount] = useState(0)
+
+  const [getNext,setNext] = useState(0)
+
+  const fetchData = ()  =>{
+    fetch('https://api.quotable.io/random')
+    .then((r)=>{
+      r.json().then((res)=>{
+        console.log(res);
+        setData({
+          author:res.author,
+          quote:res.content,
+        })
+      })
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }
+
+  useEffect(()=>{
+    fetchData()
+  },[getNext])
+
+  const clickHandler = () =>{
+    setNext(getNext + 1)
+    if(count<10){
+      setCount(count+1)
+    }
+    else{
+      setCount(0)
+    }
+  }
+
     return (
       <div id="main">
         <div id="wrapper">
-          
+          <div className='quote-text' style={{color:colors[count]}} >
+            {getData.quote}
+          </div>
+          <div className='quote-author' style={{color:colors[count]}}>
+            {getData.author}
+          </div>
+          <button onClick={clickHandler} id='new-quote'>next quote</button>
         </div>
       </div>
     );
